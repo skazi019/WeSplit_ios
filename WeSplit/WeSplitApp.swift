8e1a91c93383e739f000a4f34dd002b43ac93b7a
Kaushal Sharma <skazi019@gmail.com>
2022-02-04 17:26:57 +0530

    Initial Commit

---
+++ WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Kaushal Sharma on 04/02/22.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
