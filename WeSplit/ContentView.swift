130a1f17e3069106e8bf149cc7e47c303cdd444c
Kaushal Sharma <skazi019@gmail.com>
2022-02-06 11:23:34 +0530

    WeSplit App

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -7,10 +7,31 @@
 
 import SwiftUI
 
+extension NumberFormatter {
+    static var currency: NumberFormatter {
+        let formatter = NumberFormatter()
+        formatter.numberStyle = .currency
+        return formatter
+    }
+}
+
 struct ContentView: View {
+    @State private var checkAmount: Double = 0.0
+    @State private var numberOfPeople: Int = 2
+    @State private var tipPercentage: Int = 20
+    
+    let tipPercentages: [Int] = [10, 15, 20, 25, 0]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        Form {
+            Section {
+                TextField("Amount", value: $checkAmount, formatter: NumberFormatter.currency)
+            }
+            
+            Section {
+                Text(checkAmount, formatter: NumberFormatter.currency)
+            }
+        }
     }
 }
 
