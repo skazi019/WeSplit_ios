74504ec1394db80265270c5772f062b43761fb52
Kaushal Sharma <skazi019@gmail.com>
2022-02-19 16:11:55 +0530

    Completing the app

--- WeSplit/ContentView.swift
+++ WeSplit/ContentView.swift
@@ -11,26 +11,88 @@ extension NumberFormatter {
     static var currency: NumberFormatter {
         let formatter = NumberFormatter()
         formatter.numberStyle = .currency
+        formatter.currencyCode = Locale.current.currencyCode ?? "USD"
+        formatter.currencyGroupingSeparator = "_"
+        formatter.currencyDecimalSeparator = "."
         return formatter
     }
 }
 
+extension View {
+    func hideKeyboard() {
+        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+    }
+}
+
+extension UIViewController {
+    func hideKeyboardWhenTappedAround() {
+     let tap: UITapGestureRecognizer = UITapGestureRecognizer(target: self, action:    #selector(UIViewController.dismissKeyboard))
+      tap.cancelsTouchesInView = false
+      view.addGestureRecognizer(tap)
+    }
+    
+    @objc func dismissKeyboard() {
+       view.endEditing(true)
+    }
+}
+
 struct ContentView: View {
+//    @FocusedBinding private var isFocussed: FocusedBinding<...>?
     @State private var checkAmount: Double = 0.0
     @State private var numberOfPeople: Int = 2
     @State private var tipPercentage: Int = 20
+//    @State var hideKeyboardView = {
+//        UIApplication.shared.sendAction(#selector(UIResponder.resignFirstResponder), to: nil, from: nil, for: nil)
+//    }
+    
     
     let tipPercentages: [Int] = [10, 15, 20, 25, 0]
     
+    var totalPerPerson: Double {
+        let peopleCount = Double(numberOfPeople + 2)
+        let tipValue: Double = checkAmount / 100 * Double(tipPercentage)
+        let grandTotal: Double = checkAmount + tipValue
+        
+        let amountPerPerson = grandTotal / peopleCount
+        return amountPerPerson
+    }
+    
     var body: some View {
-        Form {
-            Section {
-                TextField("Amount", value: $checkAmount, formatter: NumberFormatter.currency)
+        NavigationView {
+            Form {
+                Section {
+                    TextField("Amount", value: $checkAmount, formatter: NumberFormatter.currency)
+                        .keyboardType(.decimalPad)
+                        .onTapGesture {
+//                            hideKeyboardWhenTappedAround()
+                        }
+                    
+                    Picker("Number of people", selection: $numberOfPeople) {
+                        ForEach(2..<100) {
+                            Text("\($0) people")
+                        }
+                    }
+                }
+
+                
+                Section(header: Text("How much tip do you want to leave?")) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
+                        ForEach(tipPercentages, id: \.self) {
+                            Text("\($0)%")
+                        }
+                    }
+                    .pickerStyle(SegmentedPickerStyle())
+                }
+                
+                Section {
+                    Text("$\(totalPerPerson, specifier: "%.2f")")
+                }
             }
-            
-            Section {
-                Text(checkAmount, formatter: NumberFormatter.currency)
-            }
+            .navigationTitle("WeSplit")
+
+//            .toolbar(content: {
+//                ToolbarItemPlacement()
+//            })
         }
     }
 }
